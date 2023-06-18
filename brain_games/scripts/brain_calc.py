048a4d0e5c9d270f93166a85bb70b33c61461be8
Shilov Ilya <shilov.spike2250@yandex.ru>
2023-06-18 16:36:41 +0500

    refactoring, add game_logic.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,59 +1,37 @@
 #!/usr/bin/env python3
-from brain_games.scripts.brain_games import greeting
+from brain_games.scripts.game_logic import start_game
 from random import randint, choice
-import prompt      
 
 
 # игра "Калькулятор"
 def calc_game():
     """
     """
-    # приветствуем пользователя и спрашиваем имя
-    user_name = greeting()
-    # выводим правила игры
-    print('What is the result of the expression?')
-    # создаем пустой список для коллекционирования правильных ответов
-    correct_answers = []
-    # 
+    # правила игры
+    rules = 'What is the result of the expression?'
+    # создаем список доступных операций
     operators = ('+', '-', '*')
-    # цикл, который завершится после 3 корректных ответов подряд
-    while correct_answers != [True, True, True]:
+    # создаем пустые списки для вопросов и правильных ответов
+    questions = []
+    correct_answers = []
+    # подготавливаем списки вопросов и правильных ответов
+    for i in range(3):
         # случайно выбираем оператор для вопроса
         operator = choice(operators)
-        # определяем 2 числа для вопроса
+        # определяем 2 числа для вопроса в заданном диапазоне
         number1 = randint(1, 25)
         number2 = randint(1, 25)
         # определяем вопрос
-        question = f"Question: {number1} {operator} {number2}"
-        #
-        correct_answer = 0
+        questions.append(f"Question: {number1} {operator} {number2}")
         # определяем правильный ответ
         if operator == '+':
-            correct_answer = number1 + number2
+            correct_answers.append(str(number1 + number2))
         elif operator == '-':
-            correct_answer = number1 - number2
+            correct_answers.append(str(number1 - number2))
         elif operator == '*':
-            correct_answer = number1 * number2
-        # выводим вопрос
-        print(question)
-        # получаем ответ
-        answer = int(prompt.string('Your answer: '))
-        # проверяем полученный ответ на правильность
-        if answer == correct_answer:
-            # выводим сообщение о корректном ответе
-            print('Correct!')
-            correct_answers.append(True)
-        else:
-            # выводим сообщение о неверном ответе
-            msg = f"'{answer}' is wrong answer ;(. "\
-                  f"Correct answer was '{correct_answer}'.\n"\
-                  f"Let's try again, {user_name}!"
-            print(msg)
-            # прерываем цикл
-            break
-    else:
-        # если цикл не прервался - выводим сообщение об успешном прохождении
-        print(f"Congratulations, {user_name}!")
+            correct_answers.append(str(number1 * number2))
+
+    start_game(rules, questions, correct_answers)
 
 
 # запускаем игру
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,50 +1,30 @@
 #!/usr/bin/env python3
-from brain_games.scripts.brain_games import greeting
+from brain_games.scripts.game_logic import start_game
 from random import randint
-import prompt
 
 
 # игра "Проверка на четность"
 def even_game():
-    '''
-    '''
-
-    # приветствуем пользователя и спрашиваем имя
-    user_name = greeting()
+    """
+    """
     # выводим правила игры
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    # создаем пустой список для коллекционирования правильных ответов
+    rules = 'Answer "yes" if the number is even, otherwise answer "no".'
+    # создаем пустые списки для вопросов и правильных ответов
+    questions = []
     correct_answers = []
     # цикл, который завершится после 3 корректных ответов подряд
-    while correct_answers != [True, True, True]:
-        # задаем новое число для вопроса с помощью randint
+    for i in range(3):
+        # задаем новое число для вопроса в заданном диапазоне
         number = randint(1, 100)
         # определяем правильный ответ
         if number % 2 == 0:
-            correct_answer = 'yes'
+            correct_answers.append('yes')
         else:
-            correct_answer = 'no'
+            correct_answers.append('no')
         # выводим вопрос
-        print(f"Question: {number}")
-        # получаем ответ
-        answer = prompt.string('Your answer: ')
-        # проверяем полученный ответ на правильность
-        if answer == correct_answer:
-            # выводим сообщение о корректном ответе
-            print('Correct!')
-            correct_answers.append(True)
-        else:
-            # выводим сообщение о неверном ответе
-            msg = f"'{answer}' is wrong answer ;(. "\
-                  f"Correct answer was '{correct_answer}'.\n"\
-                  f"Let's try again, {user_name}!"
-            print(msg)
-            # прерываем цикл
-            break
-    else:
-        # если цикл не прервался - выводим сообщение об успешном прохождении
-        print(f"Congratulations, {user_name}!")
+        questions.append(f"Question: {number}")
 
+    start_game(rules, questions, correct_answers)
 
 # запускаем игру
 def main():
